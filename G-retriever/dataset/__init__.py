5e52ac7698123821be77a562d4fede16b29a3195
sadaffatollahy <fatollahy9877@gmail.com>
2025-01-24 10:08:59 +0330

    add webqsp

--- G-retriever/dataset/__init__.py
+++ G-retriever/dataset/__init__.py
@@ -1,14 +1,15 @@
-from datasets.Explanation_graph.preprocess.expla_graphs import ExplaGraphsDataset
+from dataset.Explanation_graph.preprocess.expla_graphs import ExplaGraphsDataset
+from dataset.WebQuestionSP.preprocess.webqsp import WebQSPDataset
 # from src.dataset.scene_graphs import SceneGraphsDataset
 # from src.dataset.scene_graphs_baseline import SceneGraphsBaselineDataset
-# from src.dataset.webqsp import WebQSPDataset
+
 # from src.dataset.webqsp_baseline import WebQSPBaselineDataset
 
 
 load_dataset = {
     'expla_graphs': ExplaGraphsDataset,
+    'webqsp': WebQSPDataset,
     # 'scene_graphs': SceneGraphsDataset,
     # 'scene_graphs_baseline': SceneGraphsBaselineDataset,
-    # 'webqsp': WebQSPDataset,
     # 'webqsp_baseline': WebQSPBaselineDataset,
 }
