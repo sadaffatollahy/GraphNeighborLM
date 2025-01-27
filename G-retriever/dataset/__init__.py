9558f9ecbfa278a3238f864796f13455e2d28089
sadaffatollahy <fatollahy9877@gmail.com>
2025-01-27 10:59:21 +0330

    add cwq

--- G-retriever/dataset/__init__.py
+++ G-retriever/dataset/__init__.py
@@ -1,5 +1,6 @@
 from dataset.Explanation_graph.preprocess.expla_graphs import ExplaGraphsDataset
 from dataset.WebQuestionSP.preprocess.webqsp import WebQSPDataset
+from dataset.ComplexWebQuestions.preprocess.cwq import ComplexWebQuestions
 # from src.dataset.scene_graphs import SceneGraphsDataset
 # from src.dataset.scene_graphs_baseline import SceneGraphsBaselineDataset
 
@@ -9,6 +10,7 @@ from dataset.WebQuestionSP.preprocess.webqsp import WebQSPDataset
 load_dataset = {
     'expla_graphs': ExplaGraphsDataset,
     'webqsp': WebQSPDataset,
+    'cwq':ComplexWebQuestions
     # 'scene_graphs': SceneGraphsDataset,
     # 'scene_graphs_baseline': SceneGraphsBaselineDataset,
     # 'webqsp_baseline': WebQSPBaselineDataset,
